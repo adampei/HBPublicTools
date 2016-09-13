5c998173e014545baa7eeceb2d57f9729e2631d6
pbb <abo.x@163.com>
2016-09-13 14:44:44 +0800

    init

---
+++ HBPublicTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBPublicTools
+//
+//  Created by alex on 16/9/13.
+//  Copyright © 2016年 alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
