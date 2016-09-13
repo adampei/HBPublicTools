91f3d3f31b63a8e822a995172b4588001025cd74
pbb <abo.x@163.com>
2016-09-13 15:17:08 +0800

    add

--- HBPublicTools/HBPublicTools/NSObject+HBPublicTool.h
+++ HBPublicTools/HBPublicTools/NSObject+HBPublicTool.h
@@ -8,10 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
+
+#pragma mark - 字典数组打印
+@interface NSDictionary (Log)
+@end
+@interface NSArray (Log)
+@end
+
+
+#pragma mark - MD5加密
+@interface NSString (MyAdditions)
+- (NSString *)md5;
+@end
+
+
 @interface NSObject (HBPublicTool)
 
+/** 添加轻触手势 */
+-(void)bb_addTapGestureWithDestiView:(id)object withSelector:(SEL)selector andTarget:(id)target;
+
 /**
- *  年月日时分秒 
+ *  时间戳转换
  *
  *  @param timestamp 时间戳字符串
  *  @param format    yyyy年MM月dd日 HH:mm:ss 根据需要随意组合
--- HBPublicTools/HBPublicTools/NSObject+HBPublicTool.m
+++ HBPublicTools/HBPublicTools/NSObject+HBPublicTool.m
@@ -7,8 +7,81 @@
 //
 
 #import "NSObject+HBPublicTool.h"
+#import <CommonCrypto/CommonDigest.h>
 
+#pragma mark - 字典数组打印
+@implementation NSDictionary (Log)
+-(NSString *)descriptionWithLocale:(id)locale{
+    
+    NSMutableString *string = [NSMutableString string];
+    // 开头有个{
+    [string appendString:@"{\n"];
+    
+    // 遍历所有的键值对
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [string appendFormat:@"\t%@", key];
+        [string appendString:@" : "];
+        [string appendFormat:@"%@,\n", obj];
+    }];
+    // 结尾有个}
+    [string appendString:@"}"];
+    // 查找最后一个逗号
+    NSRange range = [string rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.location != NSNotFound)
+        [string deleteCharactersInRange:range];
+    
+    return string;
+}
+@end
+
+@implementation NSArray (Log)
+-(NSString *)descriptionWithLocale:(id)locale{
+    
+    NSMutableString *string = [NSMutableString string];
+    // 开头有个[
+    [string appendString:@"[\n"];
+    // 遍历所有的元素
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [string appendFormat:@"\t%@,\n", obj];
+    }];
+    // 结尾有个]
+    [string appendString:@"]"];
+    
+    // 查找最后一个逗号
+    NSRange range = [string rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.location != NSNotFound)
+        [string deleteCharactersInRange:range];
+    return string;
+}
+@end
+
+
+#pragma mark - MD5加密
+@implementation NSString (MyAdditions)
+- (NSString *)md5
+{
+    const char *cStr = [self UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5( cStr, (int)strlen(cStr), result ); // This is the md5 call
+    return [NSString stringWithFormat:
+            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+            result[0], result[1], result[2], result[3],
+            result[4], result[5], result[6], result[7],
+            result[8], result[9], result[10], result[11],
+            result[12], result[13], result[14], result[15]
+            ];
+}
+@end
+
+#pragma mark - NSObject分类
 @implementation NSObject (HBPublicTool)
+
+//轻触手势
+-(void)bb_addTapGestureWithDestiView:(id)object withSelector:(SEL)selector andTarget:(id)target{
+    
+    UITapGestureRecognizer * tap = [[UITapGestureRecognizer alloc] initWithTarget:target action:selector];
+    [object addGestureRecognizer:tap];
+}
 
 -(NSString *)bb_changeTimestampToString:(NSString *)timestamp withFormat:(NSString *)format{
 
